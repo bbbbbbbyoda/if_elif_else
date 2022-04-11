fca465711176813560b0d5aa1e422de09661368a
bbbbbbbyoda <tilekmam@gmail.com>
2022-04-11 13:57:18 +0600

    qwer

---
+++ p3.py
@@ -0,0 +1,9 @@
+a = 173931 % 17
+b = 546 % 17
+c = 934 % 17
+if a < b and a < c:
+	print (a)
+elif b < a and b < c: 
+	print (b)
+else:
+	print (c)
---
+++ p4.py
@@ -0,0 +1,6 @@
+x = 13
+x1 = x**2
+if x1 < 172:
+    print (x1**2)
+else:
+    print (x1)
